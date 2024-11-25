e16dc53bcfdf519bdea0fcaf77d9fb7f0c6caeb3
Syeda Momina <syedamomina848@gmail.com>
2024-11-25 21:11:47 +0500

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include "weather.h"
 
 int main() {
-    fetch_and_process_weather_data("http://api.openweathermap.org/data/2.5/weather?q=London&appid=4e095b67f35009fbcc6f055e6598c8c0");
+    fetch_and_process_weather_data("http://api.openweathermap.org/data/2.5/weather?q=London&appid=YOUR_API_KEY"); #replace it with your api key
     return 0;
 }
 
